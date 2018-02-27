749f57aa016b90eea98f089f6b46fb3599ff8264
Kitty Davies <kittyfdavies@gmail.com>
2018-02-27 21:00:58 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,14 @@
 # code your #position_taken? method here!
 def position_taken?(array, index_number)
-  if array[index_number] == " " || "" || nil
+  if array[index_number] == " "
+    false
+  elsif array[index_number] == ""
+    false
+  elsif array[index_number] == nil
     false
   else
     true
   end
 end
+
+
